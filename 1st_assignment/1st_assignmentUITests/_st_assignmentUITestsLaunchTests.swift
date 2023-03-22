d2233c62cd5abf08467931ccc2df7e866bc62336
Felix <fs146@hdm-stuttgart.de>
2023-03-22 15:37:03 +0100

    1: init empty storyboard (swift) app

---
+++ 1st_assignment/1st_assignmentUITests/_st_assignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _st_assignmentUITestsLaunchTests.swift
+//  1st_assignmentUITests
+//
+//  Created by Felix Schindler on 22.03.23.
+//
+
+import XCTest
+
+final class _st_assignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
