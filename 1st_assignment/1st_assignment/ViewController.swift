976a4c1754cf51a901d36779217f8013c3c38b12
Felix <fs146@hdm-stuttgart.de>
2023-04-05 18:31:34 +0200

    Add functionality to first assignment

--- 1st_assignment/1st_assignment/ViewController.swift
+++ 1st_assignment/1st_assignment/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var button: UIButton!
+	@IBOutlet weak var label: UILabel!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func onButtonPressed(_ sender: Any) {
+		label.text = "SwiftUI is just better."
+	}
 }
-
