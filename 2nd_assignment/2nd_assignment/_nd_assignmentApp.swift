3b95a0841fd2628498cb61e27342da0521aa8ca0
Felix <fs146@hdm-stuttgart.de>
2023-04-12 14:49:42 +0200

    Create empty 2nd_assignment app

---
+++ 2nd_assignment/2nd_assignment/_nd_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  _nd_assignmentApp.swift
+//  2nd_assignment
+//
+//  Created by Felix Schindler on 12.04.23.
+//
+
+import SwiftUI
+
+@main
+struct _nd_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
