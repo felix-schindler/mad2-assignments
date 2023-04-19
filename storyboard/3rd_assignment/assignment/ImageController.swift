3a74a6cf8361c549c4041e0c08d1a3dba0251232
Felix <fs146@hdm-stuttgart.de>
2023-04-19 14:57:23 +0200

    3rd: Move to /storyboard/

--- storyboard/3rd_assignment/assignment/ImageController.swift
+++ storyboard/3rd_assignment/assignment/ImageController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  File.swift
 //  assignment
 //
 //  Created by Felix Schindler on 19.04.23.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ImageController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
 }
-
---
+++ storyboard/3rd_assignment/assignment/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Felix Schindler on 19.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet weak var usernameField: UITextField!
+	
+	@IBOutlet weak var passwordField: UITextField!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
