539f002e04f56cb5f33ee9e848a544c4d1025870
Felix <fs146@hdm-stuttgart.de>
2023-04-19 15:07:31 +0200

    3rd: Add in SwiftUI

---
+++ 3rd_assignment/assignment/assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentApp.swift
+//  assignment
+//
+//  Created by Felix Schindler on 19.04.23.
+//
+
+import SwiftUI
+
+@main
+struct assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
