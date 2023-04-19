7eb258e186574b1210d1784a17bac86ca6c8b74c
Felix <fs146@hdm-stuttgart.de>
2023-04-19 15:23:03 +0200

    3rd (storyboard)

---
+++ 2nd_assignment/assignment/MarketList.swift
@@ -0,0 +1,20 @@
+//
+//  MarketList.swift
+//  assignment
+//
+//  Created by Felix Schindler on 19.04.23.
+//
+
+import SwiftUI
+
+struct MarketList: View {
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+struct MarketList_Previews: PreviewProvider {
+	static var previews: some View {
+		MarketList()
+	}
+}
